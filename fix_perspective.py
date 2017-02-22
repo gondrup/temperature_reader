973cfc47403c9ebe9e05a759058ec10a41cb4b78
Adrian Harding <adrian.harding@vcars.co.uk>
2017-02-22 15:51:03 +0000

    Re-structured the app a bit

--- fix_perspective.py
+++ fix_perspective.py
@@ -1,67 +1,7 @@
 #!/usr/bin/env python
 
-from PIL import Image
-from subprocess import call
-import sys
+from tempocr import imageprocess
 
 image_file = "test_images/20170209_135025.jpg"
 
-im = Image.open(image_file)
-width, height = im.size
-
-# Find coords
-
-coords = [
-    [(645,1632), (0,0)],
-    [(743,2941), (0,height)],
-    [(2600,2993), (width,height)],
-    [(2592,1494), (width,0)],
-]
-
-# Run the perspective fix with IM
-
-coords_str = ""
-for point_from, point_to in coords:
-    coords_str += str(point_from[0]) + "," + str(point_from[1])
-    coords_str += " " + str(point_to[0]) + "," + str(point_to[1]) + " "
-
-print "Coords: " + coords_str
-
-call([
-    "convert", image_file, "-matte", "-virtual-pixel", "transparent",
-    "-distort", "Perspective", coords_str,
-    "-resize", "1936x1360!",
-    "test_images/out/test.png"
-])
-
-# First attempt, didn't work, way too complicated
-'''
-from PIL import Image
-import sys
-import numpy
-
-
-def find_coeffs(pa, pb):
-    matrix = []
-    for p1, p2 in zip(pa, pb):
-        matrix.append([p1[0], p1[1], 1, 0, 0, 0, -p2[0]*p1[0], -p2[0]*p1[1]])
-        matrix.append([0, 0, 0, p1[0], p1[1], 1, -p2[1]*p1[0], -p2[1]*p1[1]])
-
-    A = numpy.matrix(matrix, dtype=numpy.float)
-    B = numpy.array(pb).reshape(8)
-
-    res = numpy.dot(numpy.linalg.inv(A.T * A) * A.T, B)
-    return numpy.array(res).reshape(8)
-
-im = Image.open("test_images/20170209_135025.jpg")
-width, height = im.size
-
-coeffs = find_coeffs(
-        [(671, 1638), (2592, 1494), (743, 2941), (2600, 2993)],
-        [(0, 0), (width, 0), (width, height), (0, height)])
-
-im.transform((width, height), Image.PERSPECTIVE, coeffs,
-        Image.BICUBIC)
-
-im.save(sys.stdout, "PNG")
-'''
+im = imageprocess.prepare_for_ocr(image_file)
--- read_temp.py
+++ read_temp.py
@@ -1,247 +1,12 @@
 #!/usr/bin/env python
 
 from PIL import Image, ImageDraw
-from lib import bresenham_line
+from tempocr import segments
 import sys
 
 # TODO: Normalize coordinates so image can be any size (reference image size was: 1936 x 1360)
 # TODO: Read file name from argv
-
-# Similarity formula taken from http://stackoverflow.com/a/26768008
-
-SIMILARITY_THRESHOLD = 18
-
-def luminance(pixel):
-    return (0.299 * pixel[0] + 0.587 * pixel[1] + 0.114 * pixel[2])
-
-def get_difference(pixel_a, pixel_b):
-    return abs(luminance(pixel_a) - luminance(pixel_b))
-
-def is_similar(pixel_a, pixel_b, threshold):
-    return get_difference(pixel_a, pixel_b) < threshold
-
-# http://stackoverflow.com/questions/19914509/python-pil-pixel-rgb-color-to-hex
-def rgb2hex(pixel):
-    return '#{:02x}{:02x}{:02x}'.format(pixel[0], pixel[1], pixel[2])
-
-class SegmentStripe:
-    def __init__(self, image, start, end):
-        self.image = image
-        self.start = start
-        self.end = end
-
-    def _get_pixels_to_check(self):
-        return bresenham_line.get_line(self.start, self.end)
-
-    def _check_pixel(self, pixel, bg_colors, fg_colors):
-        bg_values = []
-        fg_values = []
-
-        for pixel_b in bg_colors:
-            bg_values.append(get_difference(pixel, pixel_b))
-
-        for pixel_b in fg_colors:
-            fg_values.append(get_difference(pixel, pixel_b))
-
-        bg_average = sum(bg_values) / len(bg_values)
-        fg_average = sum(fg_values) / len(fg_values)
-
-        return (fg_average < bg_average)
-
-    def is_on(self, bg_colors, fg_colors):
-        on = 0
-        off = 0
-
-        for coords in self._get_pixels_to_check():
-            pixel = self.image.getpixel(coords);
-
-            if(self._check_pixel(pixel, bg_colors, fg_colors)):
-                on = on + 1
-            else:
-                off = off + 1
-
-        return (on > off)
-
-    def is_off(self, bg_colors, fg_colors):
-        return not(self.is_on(bg_colors, fg_colors))
-
-    def draw_on_image(self, fill):
-        draw = ImageDraw.Draw(self.image)
-        draw.line([self.start, self.end], fill=fill, width=2)
-        del draw
-
-class Digit:
-    def __init__(self, segment_stripes, bg_colors, fg_colors):
-        self.segment_stripes = segment_stripes
-        self.bg_colors = bg_colors
-        self.fg_colors = fg_colors
-
-    def get_digit(self):
-        return ''
-
-    def draw_on_image(self):
-        for seg in self.segment_stripes:
-            seg.draw_on_image("lightgreen")
-
-class MinusSeg(Digit):
-    def get_digit(self):
-        if(self.segment_stripes[0].is_on(self.bg_colors, self.fg_colors)): # 0 = Minus
-            return '-'
-        else:
-            return ''
-
-class TwoSeg(Digit):
-    def get_digit(self):
-        if(
-            self.segment_stripes[0].is_on(self.bg_colors, self.fg_colors) # 0 = Top
-            and self.segment_stripes[1].is_on(self.bg_colors, self.fg_colors) # 1 = Bottom
-        ):
-            return '1'
-        else:
-            return ''
-
-class PointSeg(Digit):
-    def get_digit(self):
-        if(self.segment_stripes[0].is_on(self.bg_colors, self.fg_colors)): # 0 = Dot
-            return '.'
-        else:
-            return ''
-
-class SevenSeg(Digit):
-    def _is_0(self):
-        return (
-            self.segment_stripes[0].is_on(self.bg_colors, self.fg_colors) # 0 = Top
-            and self.segment_stripes[1].is_on(self.bg_colors, self.fg_colors) # 1 = Top Right
-            and self.segment_stripes[2].is_on(self.bg_colors, self.fg_colors) # 2 = Bottom Right
-            and self.segment_stripes[3].is_on(self.bg_colors, self.fg_colors) # 3 = Bottom
-            and self.segment_stripes[4].is_on(self.bg_colors, self.fg_colors) # 4 = Bottom Left
-            and self.segment_stripes[5].is_on(self.bg_colors, self.fg_colors) # 5 = Too Left
-            and self.segment_stripes[6].is_off(self.bg_colors, self.fg_colors) # 6 = Middle
-        )
-
-    def _is_1(self):
-        return (
-            self.segment_stripes[0].is_off(self.bg_colors, self.fg_colors) # 0 = Top
-            and self.segment_stripes[1].is_on(self.bg_colors, self.fg_colors) # 1 = Top Right
-            and self.segment_stripes[2].is_on(self.bg_colors, self.fg_colors) # 2 = Bottom Right
-            and self.segment_stripes[3].is_off(self.bg_colors, self.fg_colors) # 3 = Bottom
-            and self.segment_stripes[4].is_off(self.bg_colors, self.fg_colors) # 4 = Bottom Left
-            and self.segment_stripes[5].is_off(self.bg_colors, self.fg_colors) # 5 = Too Left
-            and self.segment_stripes[6].is_off(self.bg_colors, self.fg_colors) # 6 = Middle
-        )
-
-    def _is_2(self):
-        return (
-            self.segment_stripes[0].is_on(self.bg_colors, self.fg_colors) # 0 = Top
-            and self.segment_stripes[1].is_on(self.bg_colors, self.fg_colors) # 1 = Top Right
-            and self.segment_stripes[2].is_off(self.bg_colors, self.fg_colors) # 2 = Bottom Right
-            and self.segment_stripes[3].is_on(self.bg_colors, self.fg_colors) # 3 = Bottom
-            and self.segment_stripes[4].is_on(self.bg_colors, self.fg_colors) # 4 = Bottom Left
-            and self.segment_stripes[5].is_off(self.bg_colors, self.fg_colors) # 5 = Too Left
-            and self.segment_stripes[6].is_on(self.bg_colors, self.fg_colors) # 6 = Middle
-        )
-
-    def _is_3(self):
-        return (
-            self.segment_stripes[0].is_on(self.bg_colors, self.fg_colors) # 0 = Top
-            and self.segment_stripes[1].is_on(self.bg_colors, self.fg_colors) # 1 = Top Right
-            and self.segment_stripes[2].is_on(self.bg_colors, self.fg_colors) # 2 = Bottom Right
-            and self.segment_stripes[3].is_on(self.bg_colors, self.fg_colors) # 3 = Bottom
-            and self.segment_stripes[4].is_off(self.bg_colors, self.fg_colors) # 4 = Bottom Left
-            and self.segment_stripes[5].is_off(self.bg_colors, self.fg_colors) # 5 = Too Left
-            and self.segment_stripes[6].is_on(self.bg_colors, self.fg_colors) # 6 = Middle
-        )
-
-    def _is_4(self):
-        return (
-            self.segment_stripes[0].is_off(self.bg_colors, self.fg_colors) # 0 = Top
-            and self.segment_stripes[1].is_on(self.bg_colors, self.fg_colors) # 1 = Top Right
-            and self.segment_stripes[2].is_on(self.bg_colors, self.fg_colors) # 2 = Bottom Right
-            and self.segment_stripes[3].is_off(self.bg_colors, self.fg_colors) # 3 = Bottom
-            and self.segment_stripes[4].is_off(self.bg_colors, self.fg_colors) # 4 = Bottom Left
-            and self.segment_stripes[5].is_on(self.bg_colors, self.fg_colors) # 5 = Too Left
-            and self.segment_stripes[6].is_on(self.bg_colors, self.fg_colors) # 6 = Middle
-        )
-
-    def _is_5(self):
-        return (
-            self.segment_stripes[0].is_on(self.bg_colors, self.fg_colors) # 0 = Top
-            and self.segment_stripes[1].is_off(self.bg_colors, self.fg_colors) # 1 = Top Right
-            and self.segment_stripes[2].is_on(self.bg_colors, self.fg_colors) # 2 = Bottom Right
-            and self.segment_stripes[3].is_on(self.bg_colors, self.fg_colors) # 3 = Bottom
-            and self.segment_stripes[4].is_off(self.bg_colors, self.fg_colors) # 4 = Bottom Left
-            and self.segment_stripes[5].is_on(self.bg_colors, self.fg_colors) # 5 = Too Left
-            and self.segment_stripes[6].is_on(self.bg_colors, self.fg_colors) # 6 = Middle
-        )
-
-    def _is_6(self):
-        return (
-            self.segment_stripes[0].is_on(self.bg_colors, self.fg_colors) # 0 = Top
-            and self.segment_stripes[1].is_off(self.bg_colors, self.fg_colors) # 1 = Top Right
-            and self.segment_stripes[2].is_on(self.bg_colors, self.fg_colors) # 2 = Bottom Right
-            and self.segment_stripes[3].is_on(self.bg_colors, self.fg_colors) # 3 = Bottom
-            and self.segment_stripes[4].is_on(self.bg_colors, self.fg_colors) # 4 = Bottom Left
-            and self.segment_stripes[5].is_on(self.bg_colors, self.fg_colors) # 5 = Too Left
-            and self.segment_stripes[6].is_on(self.bg_colors, self.fg_colors) # 6 = Middle
-        )
-
-    def _is_7(self):
-        return (
-            self.segment_stripes[0].is_on(self.bg_colors, self.fg_colors) # 0 = Top
-            and self.segment_stripes[1].is_on(self.bg_colors, self.fg_colors) # 1 = Top Right
-            and self.segment_stripes[2].is_on(self.bg_colors, self.fg_colors) # 2 = Bottom Right
-            and self.segment_stripes[3].is_off(self.bg_colors, self.fg_colors) # 3 = Bottom
-            and self.segment_stripes[4].is_off(self.bg_colors, self.fg_colors) # 4 = Bottom Left
-            and self.segment_stripes[5].is_off(self.bg_colors, self.fg_colors) # 5 = Too Left
-            and self.segment_stripes[6].is_off(self.bg_colors, self.fg_colors) # 6 = Middle
-        )
-
-    def _is_8(self):
-        return (
-            self.segment_stripes[0].is_on(self.bg_colors, self.fg_colors) # 0 = Top
-            and self.segment_stripes[1].is_on(self.bg_colors, self.fg_colors) # 1 = Top Right
-            and self.segment_stripes[2].is_on(self.bg_colors, self.fg_colors) # 2 = Bottom Right
-            and self.segment_stripes[3].is_on(self.bg_colors, self.fg_colors) # 3 = Bottom
-            and self.segment_stripes[4].is_on(self.bg_colors, self.fg_colors) # 4 = Bottom Left
-            and self.segment_stripes[5].is_on(self.bg_colors, self.fg_colors) # 5 = Too Left
-            and self.segment_stripes[6].is_on(self.bg_colors, self.fg_colors) # 6 = Middle
-        )
-
-    def _is_9(self):
-        return (
-            self.segment_stripes[0].is_on(self.bg_colors, self.fg_colors) # 0 = Top
-            and self.segment_stripes[1].is_on(self.bg_colors, self.fg_colors) # 1 = Top Right
-            and self.segment_stripes[2].is_on(self.bg_colors, self.fg_colors) # 2 = Bottom Right
-            and self.segment_stripes[3].is_on(self.bg_colors, self.fg_colors) # 3 = Bottom
-            and self.segment_stripes[4].is_off(self.bg_colors, self.fg_colors) # 4 = Bottom Left
-            and self.segment_stripes[5].is_on(self.bg_colors, self.fg_colors) # 5 = Too Left
-            and self.segment_stripes[6].is_on(self.bg_colors, self.fg_colors) # 6 = Middle
-        )
-
-    def get_digit(self):
-        if(self._is_0()):
-            return '0'
-        elif(self._is_1()):
-            return '1'
-        elif(self._is_2()):
-            return '2'
-        elif(self._is_3()):
-            return '3'
-        elif(self._is_4()):
-            return '4'
-        elif(self._is_5()):
-            return '5'
-        elif(self._is_6()):
-            return '6'
-        elif(self._is_7()):
-            return '7'
-        elif(self._is_8()):
-            return '8'
-        elif(self._is_9()):
-            return '9'
-        else:
-            return ''
-
+# TODO: Fix perspective and pre-crop image:
 
 def get_bg_colors(image):
     colors = []
@@ -284,42 +49,42 @@ fg_colors = get_fg_colors(rgb_image)
 # Define the known digits/segments
 
 digits = [
-    MinusSeg([
-        SegmentStripe(rgb_image, (240, 322), (220, 412)) # Check this, guessed numbers
+    segments.MinusSeg([
+        segments.SegmentStripe(rgb_image, (240, 322), (220, 412)) # Check this, guessed numbers
     ], bg_colors, fg_colors),
-    TwoSeg([
-        SegmentStripe(rgb_image, (93, 176), (114, 177)), # Check this, guessed numbers
-        SegmentStripe(rgb_image, (99, 84), (118, 86)) # Check this, guessed numbers
+    segments.TwoSeg([
+        segments.SegmentStripe(rgb_image, (93, 176), (114, 177)), # Check this, guessed numbers
+        segments.SegmentStripe(rgb_image, (99, 84), (118, 86)) # Check this, guessed numbers
     ], bg_colors, fg_colors),
-    SevenSeg([
-        SegmentStripe(rgb_image, (634, 80), (636, 170)),
-        SegmentStripe(rgb_image, (716, 240), (795, 245)),
-        SegmentStripe(rgb_image, (701, 497), (774, 503)),
-        SegmentStripe(rgb_image, (594, 573), (592, 661)),
-        SegmentStripe(rgb_image, (430, 483), (505, 493)),
-        SegmentStripe(rgb_image, (452, 232), (520, 240)),
-        SegmentStripe(rgb_image, (615, 325), (615, 413)),
+    segments.SevenSeg([
+        segments.SegmentStripe(rgb_image, (634, 80), (636, 170)),
+        segments.SegmentStripe(rgb_image, (716, 240), (795, 245)),
+        segments.SegmentStripe(rgb_image, (701, 497), (774, 503)),
+        segments.SegmentStripe(rgb_image, (594, 573), (592, 661)),
+        segments.SegmentStripe(rgb_image, (430, 483), (505, 493)),
+        segments.SegmentStripe(rgb_image, (452, 232), (520, 240)),
+        segments.SegmentStripe(rgb_image, (615, 325), (615, 413)),
     ], bg_colors, fg_colors),
-    SevenSeg([
-        SegmentStripe(rgb_image, (1045, 80), (1047, 170)),
-        SegmentStripe(rgb_image, (1127, 240), (1206, 245)),
-        SegmentStripe(rgb_image, (1112, 497), (1185, 503)),
-        SegmentStripe(rgb_image, (1005, 573), (1003, 661)),
-        SegmentStripe(rgb_image, (841, 483), (916, 493)),
-        SegmentStripe(rgb_image, (863, 232), (931, 240)),
-        SegmentStripe(rgb_image, (1026, 325), (1026, 413))
+    segments.SevenSeg([
+        segments.SegmentStripe(rgb_image, (1045, 80), (1047, 170)),
+        segments.SegmentStripe(rgb_image, (1127, 240), (1206, 245)),
+        segments.SegmentStripe(rgb_image, (1112, 497), (1185, 503)),
+        segments.SegmentStripe(rgb_image, (1005, 573), (1003, 661)),
+        segments.SegmentStripe(rgb_image, (841, 483), (916, 493)),
+        segments.SegmentStripe(rgb_image, (863, 232), (931, 240)),
+        segments.SegmentStripe(rgb_image, (1026, 325), (1026, 413))
     ], bg_colors, fg_colors),
-    PointSeg([
-        SegmentStripe(rgb_image, (1210, 614), (1211, 660)),
+    segments.PointSeg([
+        segments.SegmentStripe(rgb_image, (1210, 614), (1211, 660)),
     ], bg_colors, fg_colors),
-    SevenSeg([
-        SegmentStripe(rgb_image, (1480, 80), (1482, 170)),
-        SegmentStripe(rgb_image, (1562, 240), (1641, 245)),
-        SegmentStripe(rgb_image, (1547, 497), (1620, 503)),
-        SegmentStripe(rgb_image, (1440, 573), (1438, 661)),
-        SegmentStripe(rgb_image, (1276, 483), (1351, 493)),
-        SegmentStripe(rgb_image, (1298, 232), (1366, 240)),
-        SegmentStripe(rgb_image, (1461, 325), (1461, 413))
+    segments.SevenSeg([
+        segments.SegmentStripe(rgb_image, (1480, 80), (1482, 170)),
+        segments.SegmentStripe(rgb_image, (1562, 240), (1641, 245)),
+        segments.SegmentStripe(rgb_image, (1547, 497), (1620, 503)),
+        segments.SegmentStripe(rgb_image, (1440, 573), (1438, 661)),
+        segments.SegmentStripe(rgb_image, (1276, 483), (1351, 493)),
+        segments.SegmentStripe(rgb_image, (1298, 232), (1366, 240)),
+        segments.SegmentStripe(rgb_image, (1461, 325), (1461, 413))
     ], bg_colors, fg_colors)
 ]
 
