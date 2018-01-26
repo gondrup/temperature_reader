24bf2ae71d14134ac274942e408635a00dbe2a2f
Adrian Harding <gondrup@gmail.com>
2018-01-26 22:59:22 +0000

    Tidy code up a bit

--- test.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-
-import tesseract
-
-api = tesseract.TessBaseAPI()
-api.Init(".", "letsgodigital", tesseract.OEM_DEFAULT)
-api.SetVariable("tessedit_char_whitelist", "-+.,E1234567890")
-api.SetPageSegMode(tesseract.PSM_AUTO)
-api.SetVariable("debug_file", "/dev/null")
